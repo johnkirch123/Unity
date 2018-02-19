20c0ffcadf3010870bd1939cd71b29f9734bd9f7
John Kirch <john.kirch.business@gmail.com>
2018-02-19 03:58:14 -0700

    Random Map Generator
    2D and 3D random map generators as well as first person battle animations in Unity.

---
+++ Repositories/repos 4.7/survival-tutorial-brackeys/Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLogic : MonoBehaviour {
+
+	public int health = 100;
+	
+	void Update()
+	{
+		if (health <= 0)
+		{
+			Dead();
+		}
+	}
+	
+	void ApplyDamage(int damage)
+	{
+		health -= damage;
+	}
+	
+	void Dead()
+	{
+		Destroy(gameObject);
+	}
+}
---
+++ Repositories/repos 4.7/survival-tutorial-brackeys/Assets/Scripts/MeleeSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeSystem : MonoBehaviour {
+
+	public int damage = 50;
+	public float distance;
+	public float maxDistance = 1.5f;
+	public Transform mace;
+	
+	void Update()
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			// Attack Animation
+			mace.animation.Play("Attack");
+			//Attack function
+			RaycastHit hit;
+			if(Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out hit))
+			{
+				distance = hit.distance;
+				if(distance < maxDistance)
+				{
+					hit.transform.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		}
+		
+		if(mace.animation.isPlaying == false)
+		{
+			mace.animation.CrossFade("Idle");
+		}
+		
+		if(Input.GetKey(KeyCode.LeftShift)) 
+		{
+			mace.animation.CrossFade("Sprint");
+		}
+		
+		if(Input.GetKeyUp(KeyCode.LeftShift)) 
+		{
+			mace.animation.CrossFade("Idle");
+		}
+	}
+}
