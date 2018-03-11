23181a98a09735cdb601687af83e3adc69948486
John Kirch <john.kirch.business@gmail.com>
2018-03-11 16:08:15 -0600

    games and c# programming
    It has been a little while since I have uploaded, these files consist of C# code, gaming code, blender files.

---
+++ Repositories/repos/RPG Brakceys/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocused = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overridden.
+        Debug.Log("Interacting with " + interactionTransform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocused && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocused = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocused = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
